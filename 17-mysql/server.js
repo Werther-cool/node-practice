87922ebe784e10acf82b01c77f43bb5c84d1f928
Werther <zhouwangyulu@icloud.com>
2017-09-21 15:51:30 +0800

    node

---
+++ 17-mysql/server.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+
+var db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"123456",
+    database:"user_test"
+})
+
+db.query("SELECT * FROM `user_table`;",(err,data)=>{
+    if (err) {
+        console.log('出错了',err);
+    }else{
+        console.log('成功了',data);
+    }
+})
