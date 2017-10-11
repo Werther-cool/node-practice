f04dbcac49b29eb97a31fcea98a98be432334fa2
Werther <zhouwangyulu@icloud.com>
2017-10-11 17:29:44 +0800

    node

--- 19-wholestack/server.js
+++ 19-wholestack/server.js
@@ -6,3 +6,30 @@ const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
 const consolidate = require('consolidate');
 const expressRoute = require('express-route');
+ 
+var server = express();
+server.listen(8080);
+
+// 1 获取请求数据
+// get自带
+server.use(multerObj.any());
+
+
+// 2. cookie,session
+server.use(cookieParser());
+(function () {
+    var key = [];
+    for(var i = 0;i<100000;i++){
+        keys[i]='a_'+Math.random();
+    }
+    server.use(cookieSession({
+        name:'sess_id',
+        keys:keys,
+        maxAge:20*60*1000
+    }))
+    
+})
+
+// 3. 模板
+server.engine('html',consolidate.ejs);
+server.set
