c6780b12081316a3eb0cf007f1922cf1b2cca751
Werther <zhouwangyulu@icloud.com>
2017-10-19 09:50:44 +0800

    node

---
+++ 19-wholestack/md5.js
@@ -0,0 +1,7 @@
+const common = require('./libs/common');
+
+var str ='123456';
+
+var str = common.md5(str+"asdkmfkamsdkfm");
+
+console.log(str);
--- 19-wholestack/route/admin.js
+++ 19-wholestack/route/admin.js
@@ -1,7 +1,12 @@
 const express=require('express');
+const common = require('../libs/common');
+const mysql = require('mysql');
+
+var db = mysql.createPool({host:'localhost',user:'root',password:'123456',database:'`blog`'});
 
 module.exports=function (){
   var router=express.Router();
+
 
   //检查登录状态
   router.use((req, res, next)=>{
@@ -12,10 +17,36 @@ module.exports=function (){
     }
   });
 
-  //
+  router.post('/login',(req,res)=>{
+    var username = req.body.username;
+    var password = common.md5(req.body.passwor+common.MD5_SUFFIX);
+
+    db,query(`SELECT * FROM admin_table WHERE username='${username}'`,(err,data)=>{
+      if (err) {
+        console.error(err);
+        res.status(500).send('database error').end();
+      }else{
+        if (data.length==0) {
+          res.status(400).send('no this admin').end();
+        }else{
+          if (data[0].password==password) {
+            // 成功
+            res.session['admin_id'] = data[0].ID;
+            res.redirect('/admin/');
+          }else{
+            res.status(400).send('this.password is incorrect').end();
+          }
+        }
+      }
+    })
+
+  })
+
+
   router.get('/login', (req, res)=>{
     res.render('admin/login.ejs', {});
   });
+
 
   return router;
 };
