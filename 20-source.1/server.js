510e952bde5ee7e8a1faf81dcf770e00f3111fee
Werther <zhouwangyulu@icloud.com>
2017-10-30 09:24:56 +0800

    node

--- 20-source.1/server.js
+++ 20-source.1/server.js
@@ -1,44 +1,48 @@
-const express=require('express');
-const static=require('express-static');
-const bodyParser=require('body-parser');
-const multer=require('multer');
-const multerObj=multer({dest: './static/upload'});
-const mysql=require('mysql');
-const cookieParser=require('cookie-parser');
-const cookieSession=require('cookie-session');
-const consolidate=require('consolidate');
-const expressRoute=require('express-route');
+const express = require('express');
+const static = require('express-static');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const multerObj = multer({dest:'./static/upload'});
+const mysql = require('mysql');
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+const consolidate = require('consolidate');
+const expressRoute = require('express-route');
 
 var server=express();
 server.listen(8080);
 
-//1.获取请求数据
-//get自带
-server.use(bodyParser.urlencoded());
+// 1 获取请求数据
+server.use((bodyParser.urlencoded));
 server.use(multerObj.any());
 
-//2.cookie、session
+// 2 cookie session
 server.use(cookieParser());
-(function (){
+(function () {
   var keys=[];
   for(var i=0;i<100000;i++){
-    keys[i]='a_'+Math.random();
+    keys[i] = 'a_' + Math.random();
   }
   server.use(cookieSession({
-    name: 'sess_id',
-    keys: keys,
-    maxAge: 20*60*1000  //20min
-  }));
+    name:"sess_id",
+    keys:keys,
+    maxAge: 20*60*1000
+  }))
 })();
 
-//3.模板
-server.engine('html', consolidate.ejs);
-server.set('views', 'template');
-server.set('view engine', 'html');
+// 3 模板
+server.engine('html',consolidate.ejs);
+server.set('views','template');
+server.set('view engine','html');
 
-//4.route
-server.use('/', require('./route/web.js')());
-server.use('/admin/', require('./route/admin/index.js')());
+// 4 route
+server.use('/',require('./route/web.js')());
+server.use('/admin/',require('./route/admin/index.js')());
 
-//5.default：static
-server.use(static('./static/'));
+// 5 static
+server.use(static('./static'));
+
+
+
+
+
